f3fc78dbb0381ba07a438d7123ffc9bdc86fd1ec
rbudania <42476520+rbudania@users.noreply.github.com>
2020-03-03 19:21:53 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, {
+//         fullWidth: true
+//     });
+// });
